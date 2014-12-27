535e47e44ea4d11f31c579277d9073255210e13f
Saad <saadzaman@outlook.com>
2014-12-27 09:52:54 +0500

    removed some useless code

--- protected/controllers/TicketController.php
+++ protected/controllers/TicketController.php
@@ -115,29 +115,11 @@ class TicketController extends Controller
 		));
 	}
 
-	/**
-	 * Deletes a particular model.
-	 * If deletion is successful, the browser will be redirected to the 'admin' page.
-	 * @param integer $id the ID of the model to be deleted
-	 
-	public function actionDelete($id)
-	{
-		$this->loadModel($id)->delete();
-
-		// if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
-		if(!isset($_GET['ajax']))
-			$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
-	}
-
-
-
-        
-        
-         * 
-         */
 	
+	/* This function is used for AJAX call of fetch*/
         public function actionValidateTicket() 
         {
+            
             $ticket_no = Yii::app()->request->getParam('ticket_no');
             $model = Ticket::getModelByTicketNo($ticket_no);
           
@@ -162,6 +144,7 @@ class TicketController extends Controller
             {
                  $status = 'UNPAID';
             }
+            
             echo CJSON::encode(array('status'=>$status,
                                     'sold_with_promotion_id' => $sold_with_promotion_id,  
                                     'attended' => $attended,
@@ -173,60 +156,6 @@ class TicketController extends Controller
                         
         }
         
-        /**
-	 * Lists all models.
-	 */
-	 
-	public function actionIndex()
-	{
-		$dataProvider=new CActiveDataProvider('Ticket');
-		$this->render('index',array(
-			'dataProvider'=>$dataProvider,
-		));
-	}
-
-	/**
-	 * Manages all models.
-	 
-	public function actionAdmin()
-	{
-		$model=new Ticket('search');
-		$model->unsetAttributes();  // clear any default values
-		if(isset($_GET['Ticket']))
-			$model->attributes=$_GET['Ticket'];
-
-		$this->render('admin',array(
-			'model'=>$model,
-		));
-	}
-*/
-	/**
-	 * Returns the data model based on the primary key given in the GET variable.
-	 * If the data model is not found, an HTTP exception will be raised.
-	 * @param integer $id the ID of the model to be loaded
-	 * @return Ticket the loaded model
-	 * @throws CHttpException
-	 */
-	public function loadModel($id)
-	{
-		$model=Ticket::model()->findByPk($id);
-		if($model===null)
-			throw new CHttpException(404,'The requested page does not exist.');
-		return $model;
-	}
-
-	/**
-	 * Performs the AJAX validation.
-	 * @param Ticket $model the model to be validated
-	 */
-	protected function performAjaxValidation($model)
-	{
-		if(isset($_POST['ajax']) && $_POST['ajax']==='ticket-form')
-		{
-			echo CActiveForm::validate($model);
-			Yii::app()->end();
-		}
-	}
 	
 	public static function getModelByTicketNumber($ticket_no)
 	{
