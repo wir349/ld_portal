7073b670dde9d2945a04b9bbcba1f3350915219c
Saad <saadzaman@outlook.com>
2014-12-30 23:32:45 +0500

    mmai

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -31,9 +31,10 @@
 	<div id="mainmenu">
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
-				array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-				array('label'=>'Contact', 'url'=>array('/site/contact')),
+				
+				
+				array('label'=>'Sign In', 'url'=>array('/ticket/update')),
+                                array('label'=>'Enter User Data', 'url'=>array('/attendee/create')),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
 			),
